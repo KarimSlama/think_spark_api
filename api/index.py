a2f8d6f338d606dcd3c04cf3c25e623e3cc9e022
karim slama <karimslama917@gmail.com>
2025-11-29 02:28:52 -0800

    Fix Vercel: use api/index.py as entry point instead of wsgi.py directly

--- api/index.py
+++ api/index.py
@@ -10,5 +10,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 # Import Django WSGI application
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+# Vercel expects 'handler' variable
+handler = get_wsgi_application()
+# Also export as 'application' for compatibility
+application = handler
 
